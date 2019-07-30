47402ebc0bcab563c6797a9df7e12dac31e47905
coco <courtneyk1089@gmail.com>
2019-07-30 18:51:13 -0400

    ajax requests for login, logout, signup are working on the window

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -17,7 +17,7 @@ class Api::SessionsController < ApplicationController
         @user = current_user
         if @user
             logout
-            render {}
+            render json: {}
         else
             render json: ["No one is signed in"], status: 404
         end
--- frontend/curated.jsx
+++ frontend/curated.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { login, logout, signup } from './util/session_api.util';
 
 document.addEventListener("DOMContentLoaded", () => {
     let root = document.getElementById("root");
+    window.login = login;
+    window.logout = logout;
+    window.signup = signup;
     ReactDOM.render(<h1>Welcome to Curated</h1>, root);
 });
 
