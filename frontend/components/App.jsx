815fb16dd8dd599892223dd17de2d068feaa2247
coco <courtneyk1089@gmail.com>
2019-07-31 20:21:29 -0400

    user auth working. errors rendering. bootstrap user. auth routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,16 +3,24 @@ import GreetingContainer from './greeting_container';
 import LoginFormContainer from './login_form_container';
 import SignupFormContainer from './signup_form_container';
 import {Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom'
+import { AuthRoute, ProtectedRoute } from '../util/route_util'
 
 const App = () => (
-    <div>
-        <header>
+    <div className="background-grid">
+    <div className="overlay">
+        {/* <header>
             <h1>curated</h1>
-            {/* <GreetingContainer /> */}
-        </header>
-      
-        <Route path="/login" component={LoginFormContainer} />
-        <Route path="/signup" component={SignupFormContainer} />
+            <GreetingContainer />
+        </header> */}
+        <Switch>
+            <Route exact path="/" component={SignupFormContainer} />
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
+            <AuthRoute exact path="/login" component={LoginFormContainer} />
+            <ProtectedRoute path="/index" component={GreetingContainer} />
+        </Switch>
+        
+        
+        </div>
     </div>
 );
 
--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -32,7 +32,8 @@ class SessionForm extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
         const user = Object.assign({}, this.state);
-        this.props.processForm(user);
+        this.props.processForm(user)
+        .then(()=> this.props.history.push('/index'))
     }
 
     renderErrors() {
