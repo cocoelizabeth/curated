7d8f8f7616817592e7403501b2c125c5704b1889
coco <courtneyk1089@gmail.com>
2019-08-01 21:20:22 -0400

    fix session errors

--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -34,7 +34,7 @@ class SessionForm extends React.Component {
     renderErrors() {
         return(
             <ul>
-                {this.props.errors.map((error, i) => (
+                {this.props.errors.session.map((error, i) => (
                     <li key={`error-${i}`}>
                         {error}
                     </li>
